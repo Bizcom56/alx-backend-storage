3c4f9ae5d61aae9b82fb4e9c34b20f1eb35cc5ed
“Bizcom56” <dornorjoshua56@gmail.com>
2024-08-14 10:11:24 -0700

    Redis and Python exercise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,22 +8,41 @@ import redis
 
 
 def count_calls(method: Callable) -> Callable:
-    """a decorator script that takes a single method Callable argument
+    """decorator that takes a single method Callable argument
     and returns a Callable"""
     key = method.__qualname__
 
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """an increments the count for that key every time the method
+        """increments the count for that key every time the method
         is called and returns the value returned by the original method """
         self._redis.incr(key)
         return method(self, *args, **kwargs)
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """stores the history of inputs and outputs for a particular function
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """saves the input and output of each function in redis
+        """
+        input_key = method.__qualname__ + ":inputs"
+        output_key = method.__qualname__ + ":outputs"
+
+        output = method(self, *args, **kwargs)
+
+        self._redis.rpush(input_key, str(args))
+        self._redis.rpush(output_key, str(output))
+
+        return output
+
+    return wrapper
+
 
 def replay(fn: Callable):
-    """a script that displays the history of calls of a particular function"""
+    """Display the history of calls of a particular function"""
     r = redis.Redis()
     f_name = fn.__qualname__
     n_calls = r.get(f_name)
@@ -92,23 +111,3 @@ class Cache():
         except Exception:
             variable = 0
         return variable
-
-def call_history(method: Callable) -> Callable:
-    """a script that stores the history of inputs and outputs for a particular function
-    """
-    @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        """saves the input and output of each function in redis
-        """
-        input_key = method.__qualname__ + ":inputs"
-        output_key = method.__qualname__ + ":outputs"
-
-        output = method(self, *args, **kwargs)
-
-        self._redis.rpush(input_key, str(args))
-        self._redis.rpush(output_key, str(output))
-
-        return output
-
-    return wrapper
-
